0c660d49f6f5ca37295cf28cfabb44a3322084a3
Sulaiman <suleiman_hayek@yahoo.com>
2021-12-12 07:24:30 +0300

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
+
 function App() {
   return (
+    <>
+    <Particles className="particles-canvas"
+    params={{
+      Particles:{
+        Number:{
+          value:30,
+          density:{
+            enable:true,
+            value_area:900
+          }
+        },
+        shape: {
+          type:"circle",
+          stroke: {
+            width: 6,
+            color: "#f9ab00",
+
+          }
+        }
+      }
+    }}
+    />
     <Navbar></Navbar>
+    <Header></Header>
+
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactTyped from "react-typed"
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>We make your dashboards lit</h1>
+                <ReactTyped 
+                className="typed-text"
+                strings={["Ready to use themes", "Trainings", "Customized pages"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+                />
+                <a href="#" className="btn-main-offer">Contact us</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
